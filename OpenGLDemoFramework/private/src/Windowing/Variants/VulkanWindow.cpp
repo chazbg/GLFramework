574077363b2c9a679fa790e9f0fb4e712e81c1b7
Dian Nikolov <dian.nikolov91@gmail.com>
2016-03-31 22:01:28 +0300

    Added Vulkan enumerate and device steps

--- OpenGLDemoFramework/private/src/Windowing/Variants/VulkanWindow.cpp
+++ OpenGLDemoFramework/private/src/Windowing/Variants/VulkanWindow.cpp
@@ -3,6 +3,16 @@
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_sdk_platform.h>
 #include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <vector>
+
+#if defined(NDEBUG) && defined(__GNUC__)
+#define U_ASSERT_ONLY __attribute__((unused))
+#else
+#define U_ASSERT_ONLY
+#endif
+
 VulkanWindow::VulkanWindow(const WindowParameters & params, IApplication & app) : params(params), app(app)
 {
 
@@ -43,6 +53,59 @@ void VulkanWindow::startRenderLoop()
         std::cout << "unknown error\n";
         exit(-1);
     }
+
+    uint32_t gpu_count = 1;
+    res = vkEnumeratePhysicalDevices(inst, &gpu_count, NULL);
+    assert(gpu_count);
+    std::vector<VkPhysicalDevice> gpus;
+    gpus.resize(gpu_count);
+    res = vkEnumeratePhysicalDevices(inst, &gpu_count, gpus.data());
+    assert(!res && gpu_count >= 1);
+
+    VkDeviceQueueCreateInfo queue_info = {};
+    uint32_t queue_count;
+
+    vkGetPhysicalDeviceQueueFamilyProperties(gpus[0], &queue_count, NULL);
+    assert(queue_count >= 1);
+
+    std::vector<VkQueueFamilyProperties> queue_props;
+    queue_props.resize(queue_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(gpus[0], &queue_count, queue_props.data());
+    assert(queue_count >= 1);
+
+    bool found = false;
+    for (unsigned int i = 0; i < queue_count; i++) {
+        if (queue_props[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            queue_info.queueFamilyIndex = i;
+            found = true;
+            break;
+        }
+    }
+    assert(found);
+    assert(queue_count >= 1);
+
+    float queue_priorities[1] = { 0.0 };
+    queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queue_info.pNext = NULL;
+    queue_info.queueCount = 1;
+    queue_info.pQueuePriorities = queue_priorities;
+
+    VkDeviceCreateInfo device_info = {};
+    device_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    device_info.pNext = NULL;
+    device_info.queueCreateInfoCount = 1;
+    device_info.pQueueCreateInfos = &queue_info;
+    device_info.enabledExtensionCount = 0;
+    device_info.ppEnabledExtensionNames = NULL;
+    device_info.enabledLayerCount = 0;
+    device_info.ppEnabledLayerNames = NULL;
+    device_info.pEnabledFeatures = NULL;
+
+    VkDevice device;
+    res = vkCreateDevice(gpus[0], &device_info, NULL, &device);
+    assert(res == VK_SUCCESS);
+
+    vkDestroyDevice(device, NULL);
 
     vkDestroyInstance(inst, NULL);
 }
